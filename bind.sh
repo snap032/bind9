b3a8f25314380add12fda44ee09514e67d10bfa9
snap032 <44320435+snap032@users.noreply.github.com>
2019-02-19 01:38:47 +0200

    Add files via upload

---
+++ bind.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo apt-get install bind9 -y
+
+#Генерируем ключ
+dnssec-keygen -a HMAC-MD5 -b 128 -r /dev/urandom -n USER DHCP_UPDATER
+#Выводим в консоль ключ
+cat Kdhcp_updater.*.private|grep Key
+#Делаем переменую
+key=${cat Kdhcp_updater.*.private|grep Key}
+sudo tee -a /etc/resolvconf/resolv.conf.d/tail << EOF
+
+domain first_name.net
+search nameDevOps.org
+nameserver 127.0.0.1
+EOF
+
+
+mkdir /etc/rndc.key/
+git clone https://github.com/snap032/network_epam.git
+cp network_epam/rndc.conf /etc/rndc.key/
+cp network_epam/named.conf /etc/rndc.key/
+
+sudo cp /etc/bind/db.local /var/lib/bind/prime_zone
+
+
+
+
+
