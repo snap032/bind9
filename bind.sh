c01e4595de5e1273c09832de24a06686bae9d02d
snap032 <44320435+snap032@users.noreply.github.com>
2019-02-19 09:20:49 +0200

    Update bind.sh

--- bind.sh
+++ bind.sh
@@ -17,9 +17,9 @@ EOF
 
 
 mkdir /etc/rndc.key/
-git clone https://github.com/snap032/network_epam.git
-cp network_epam/rndc.conf /etc/rndc.key/
-cp network_epam/named.conf /etc/rndc.key/
+git clone https://github.com/snap032/bind9.git
+cp bind9/rndc.conf /etc/rndc.key/
+cp bind9/named.conf /etc/rndc.key/
 
 sudo cp /etc/bind/db.local /var/lib/bind/prime_zone
 
